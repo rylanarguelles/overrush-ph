42f38ff90244f89414dc3c3575fb9383d5054de5
Rylan Arguelles <hideyarguelles@Rylan.local>
2020-04-09 01:37:03 +1000

    Add appbar component to main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import OverrushAppBar from './components/universal/OverrushAppBar';
 
 export default class App extends React.Component {
     render() {
-        return <div className='App'>Something!</div>;
+        return (
+            <Grid container direction='column'>
+                <Grid item>
+                    <OverrushAppBar />
+                </Grid>
+            </Grid>
+        );
     }
 }
